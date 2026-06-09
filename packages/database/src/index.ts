a27ec778f8edaed41d48c2874bfc828e2e043a37
thunder <62392971+Thunder-077@users.noreply.github.com>
2026-06-09 19:07:02 +0800

    Fix database package postgres entry

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,2 +1,2 @@
-export { createScopedPrismaClient, getPrismaClient, prisma, default } from "./client"
+export { createScopedPrismaClient, getPrismaClient, prisma, default } from "./client.postgres"
 export { Prisma } from "@prisma/client"
